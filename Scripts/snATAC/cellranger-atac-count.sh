9332ac318ba6f44eca33bfe1940cf199f33a336c
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-02-05 15:36:27 -0600

    Create cellranger-atac-count.sh

---
+++ Scripts/snATAC/cellranger-atac-count.sh
@@ -0,0 +1,28 @@
+# module load cellrangerATAC/2.0.0
+$ cellranger-atac count --id=NA104
+                        --reference=/work/Neuroinformatics_Core/s204365/References/refdata-cellranger-arc-mm10-2020-A-2.0.0
+                        --fastqs=/work/Neuroinformatics_Core/s204365/ATACSeq_0002/SAMPLE104_01_02
+                        --sample=NA104
+                        --localcores=8
+                        --localmem=64
+                        
+ $ cellranger-atac count --id=NA105
+                         --reference=/work/Neuroinformatics_Core/s204365/References/refdata-cellranger-arc-mm10-2020-A-2.0.0
+                         --fastqs=/work/Neuroinformatics_Core/s204365/ATACSeq_0002/SAMPLE105_01_02
+                         --sample=NA105
+                         --localcores=8
+                         --localmem=64
+ 
+ $ cellranger-atac count --id=NA106
+                         --reference=/work/Neuroinformatics_Core/s204365/References/refdata-cellranger-arc-mm10-2020-A-2.0.0
+                         --fastqs=/work/Neuroinformatics_Core/s204365/ATACSeq_0002/SAMPLE106_01_02
+                         --sample=NA106
+                         --localcores=8
+                         --localmem=64
+ 
+ $ cellranger-atac count --id=NA107
+                         --reference=/work/Neuroinformatics_Core/s204365/References/refdata-cellranger-arc-mm10-2020-A-2.0.0
+                         --fastqs=/work/Neuroinformatics_Core/s204365/ATACSeq_0002/SAMPLE107_01_02
+                         --sample=NA107
+                         --localcores=8
+                         --localmem=64
